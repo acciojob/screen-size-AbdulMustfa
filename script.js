4ddb9f796842499600dc4bd48dfef39c9952158a
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-06-10 12:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+    function updateWindowSize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const sizeText = `Width: ${width} Height: ${height}`;
+      document.getElementById('sizeText').textContent = sizeText;
+    }
+
+window.addEventListener('resize', updateWindowSize);
+    updateWindowSize();
+ setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 3000);
